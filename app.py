d0b6e53449ae64447cd8309cc96d3d842d34926e
Nicholas Noronha <nicholascaio@hotmail.com>
2022-12-19 20:53:29 -0300

    feat(app): config flask app, JWT, swagger blueprints, and db

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+
+from flask import Flask
+from flask_smorest import Api
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+
+from db import db
+# from blocklist import BLOCKLIST #! need column 'insert only'
+import models #!
+
+from resources.user import blp as UserBlueprint
+
+def create_app(db_url=None):
+    app = Flask(__name__)
+    load_dotenv()
+
+    app.config["PROPAGATE_EXCEPTIONS"] = True 
+
+    app.config["API_TITLE"] = "Fine Hour API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3" 
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
+    db.init_app(app)
+    api = Api(app)
+
+    app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY", "testtest")
+    jwt = JWTManager(app)
+
+    with app.app_context():
+        db.create_all()
+
+    api.register_blueprint(UserBlueprint)
+
+    return app
--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    password = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     qtd_horas = db.Column(db.Integer)
     valor_horas = db.Column(db.Float(2))
