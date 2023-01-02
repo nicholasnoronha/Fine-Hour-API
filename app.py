37ab674c4621a6c8e1650b2f20a821323e156cd3
Nicholas Noronha <nicholascaio@hotmail.com>
2023-01-02 20:46:12 -0300

    feat(jwt): add the expired and invalid token loaders that where missing

--- app.py
+++ app.py
@@ -37,6 +37,22 @@ def create_app(db_url=None):
     with app.app_context():
         db.create_all()
 
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401
+        )
+
     @jwt.unauthorized_loader
     def missing_token_callback(error):
         return(
